5c1dd467c4d3afec11a4641806487afe0b933e6e
Piotr Kożuchowski <p.kozuchowski@polsource.com>
2020-05-17 19:30:49 +0200

    init

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # ss-shaders
+
+Custom Star Sonata 2 ship shaders which makes all ships to be illuminated from the top, which gives them "C1" / sprite feeling.
+
+#Installation
+Download [mod package](/amorek/name/releases/latest/download/spritelike-shaders-1-0.zip) into Star Sonata 2 "Content" directory.
+
+#Known Issues
+* Lion ship does not use ship shader (game bug)
+* Some stations appear to be black (base model has faces pointing to it's inside)
+
+
+#Screenshots
+!(/images/Screenshot_1.png)
+!(/images/Screenshot_2.png)
+!(/images/Screenshot_3.png)
+!(/images/Screenshot_4.png)
---
+++ src/shaders/ShipMaterialP3.hlsl
@@ -0,0 +1,67 @@
+#define EFFECT_MAX_LIGHT_COUNT 8
+
+sampler2D ColoredTextureSampler : register(s0);
+sampler2D NormalMapSampler : register(s1);
+sampler2D EnvMapSampler : register(s2);
+
+float3 LightPositions[EFFECT_MAX_LIGHT_COUNT];
+float LightAtts[EFFECT_MAX_LIGHT_COUNT];
+float3 LightColors[EFFECT_MAX_LIGHT_COUNT];
+float3 SpecularColors[EFFECT_MAX_LIGHT_COUNT];
+float3 AmbientColor;
+float4 DiffuseTint;
+float SpecularExponent, LCount, DoSphereMapping;
+bool UseNormalMap;
+
+static const float3 lDir = float3(0,0,1);
+
+
+float3 getLightContribution(float3 Normal)
+{   
+	float diffuse = saturate(dot(Normal, lDir));
+	float3 Reflect = normalize(4.0 * diffuse * Normal - lDir);
+	float specFac = clamp(dot(Reflect, lDir),0, 0.97);
+	float specular = pow(specFac, SpecularExponent);
+
+	return (diffuse + specular);
+}
+
+float4 pixelMain
+(
+   float2 TexCoords        : TEXCOORD0,
+   float3 View             : TEXCOORD1,
+   float3 Normal           : TEXCOORD2,
+   float3 Tangent          : TEXCOORD3,
+   float3 Binormal         : TEXCOORD4,
+   float3 WPos             : TEXCOORD5,
+   float4 Misc		 		: TEXCOORD6,
+   float4 Misc2	 			: TEXCOORD7
+) 
+   : COLOR0
+{
+	if (UseNormalMap) {
+		float4 sample = tex2D(NormalMapSampler, TexCoords);
+		float3 peturbation = sample.xyz - float3(0.5, 0.5, 0.5);
+		Normal = float3(
+			peturbation.x * Tangent - 
+			peturbation.y * Binormal + 
+			peturbation.z * Normal);
+	}
+	/* Interpolated vertex normals might not be normalized. */
+	Normal = normalize(Normal);
+
+	float4 albedoColor = tex2D(ColoredTextureSampler, TexCoords) * DiffuseTint;
+
+	if(DoSphereMapping > 0.5)
+	{
+		float2 refCoords = reflect(View, Normal).xy * 0.5;
+		albedoColor = lerp(albedoColor, tex2D(EnvMapSampler, refCoords), 0.5);
+	}	
+
+	float3 finalLightColor = getLightContribution(Normal);
+
+	float4 finalColor = saturate(albedoColor * float4(finalLightColor.xyz, 1.0));
+	finalColor.a = saturate(DiffuseTint.a);
+
+	return finalColor;
+}
